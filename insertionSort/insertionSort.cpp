877c5515ed851fe3de168077c67ccbd6c6842c5a
dewikhulbi <dewikhlb20@gmail.com>
2023-03-23 14:46:21 +0700

    Add project files.

---
+++ insertionSort/insertionSort.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int arr[20];   //membuat array dengan panjang data 20
+int n;          //membuat variable inputan n
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+
